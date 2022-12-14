9f0e031acdae498e2ba96ab7d9a05fa5b63ad2b6
Klajdi660 <64031806+Klajdi660@users.noreply.github.com>
2022-12-14 22:27:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import {useState} from 'react';
 import AddTaskForm from './components/AddTaskForm.jsx';
 import UpdateForm from './components/UpdateForm.jsx';
 import ToDo from './components/ToDo.jsx';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './App.css';
 
 function App() {
-
   // Tasks (ToDo List) State
   const [toDo, setToDo] = useState([]);
-  console.log('toDo :>> ', toDo);
+  
   // Temp State
   const [newTask, setNewTask] = useState('');
   const [updateData, setUpdateData] = useState('');
-  console.log('updateData :>> ', updateData);
+  
   // Add task 
-  ///////////////////////////
+  
   const addTask = () => {
     if(newTask) {
       let num = toDo.length + 1; 
@@ -28,14 +25,12 @@ function App() {
   }
 
   // Delete task 
-  ///////////////////////////
   const deleteTask = (id) => {
     let newTasks = toDo.filter( task => task.id !== id)
     setToDo(newTasks);
   }
 
   // Mark task as done or completed
-  ///////////////////////////
   const markDone = (id) => {
     let newTask = toDo.map( task => {
       if( task.id === id ) {
@@ -47,42 +42,32 @@ function App() {
   }
 
   // Cancel update
-  ///////////////////////////
   const cancelUpdate = () => {
     setUpdateData('');
   }
 
   // Change task for update
-  ///////////////////////////
   const changeTask = (e) => {
     let newEntry = {
       id: updateData.id,
       title: e.target.value,
     }
-    console.log('updateData.id :>> ', updateData.id);
     setUpdateData(newEntry);
-    console.log('newEntry :>> ', newEntry);
   }
 
   // Update task
-  ///////////////////////////
   const updateTask = () => {
     let filterRecords = [...toDo].filter((task) => task.id !== updateData.id );
-    console.log('task :>> ', toDo);
     let updatedObject = [...filterRecords, updateData]
-    console.log('updateObject :>> ', updatedObject);
-    console.log('updateData :>> ', updateData);
     setToDo(updatedObject);
     setUpdateData('');
   }
 
   return (
     <div className="container App">
-
     <br /><br />
     <h2>To Do List App (ReactJS)</h2>
     <br /><br />
-
     {updateData && updateData ? (
       <UpdateForm 
         updateData={updateData}
@@ -99,16 +84,13 @@ function App() {
     )}
 
     {/* Display ToDos */}
-
     {toDo && toDo.length ? '' : 'No Tasks...'}
-
     <ToDo
       toDo={toDo}
       markDone={markDone}
       setUpdateData={setUpdateData}
       deleteTask={deleteTask}
     />  
-
     </div>
   );
 }
